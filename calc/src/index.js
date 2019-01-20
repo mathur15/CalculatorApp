5ef55bd67fa6b1184ef2a997cc911aa7eae1cb40
SomMathur <som.mathur@mail.utoronto.ca>
2019-01-20 01:57:19 -0500

    Calculator made. Add logic

--- calc/src/index.js
+++ calc/src/index.js
@@ -1,35 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
+
 
 /*Steps to create
 1. Create a component to display entire calculator(result+keys)
 2. Create another component just for result
 3. Pass in the state of calculator to result for it display
 */
-class result extends React.Component{
 
+function Square(){
+          return(
+               <button className="calcButtons">
+               BUTTON!!
+              </button>
+          );
 }
 
+// class result extends React.Component{
+
+// }
+
 class calculator extends React.Component{
+     renderSquare(i){
+          return(
+               <Square/>
+          )
+     }
+
+     render(){
+          return(
+           <div>
+               <div className="boardRow">
+                   {this.renderSquare(1)}
+                   {this.renderSquare(1)}
+                   {this.renderSquare(1)}
+                   {this.renderSquare(1)}
+               </div>
+               <div className="boardRow">
+                    {this.renderSquare(1)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(1)}
+               </div>
+               <div className="boardRow">
+                    {this.renderSquare(1)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(1)}
+               </div>
+          </div>
+          )
+     }
 
 }
 
 
 class calc extends React.Component{
-	render(){
-		return(
-			<div className="outer">
-				<div className="calculator">
-					<calculator/>
-				</div>
-				<div className="info">
-					<div></div>
-				</div>
-			<div>);
-	}
+     render(){
+          return(
+               <div className="outer">
+                    <div className="calculator">
+                         <calculator/>
+                    </div>
+                    <div className="info">
+                         <div></div>
+                    </div>
+               </div>
+          );
+     }
 }
+
 ReactDOM.render(
-	<calc />),
-	document.getElementById('root')
-	);
+     <calc />,
+     document.getElementById('root')
+);
